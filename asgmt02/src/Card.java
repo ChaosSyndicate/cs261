27ad4c1451dd9d4c95b8aec025cea7882660dd22
Kevin Bui <bui.kevin@live.com>
2014-05-04 02:25:49 -0700

    Implement the ability to calculate the score of the card

--- asgmt02/src/Card.java
+++ asgmt02/src/Card.java
@@ -2,13 +2,33 @@ public class Card
 {
 	public enum Rank
 	{
-		ACE, KING, QUEEN, JACK, TEN, NINE, EIGHT,
-		SEVEN, SIX, FIVE, FOUR, THREE, DEUCE
+		ACE(1), KING(2), QUEEN(3), JACK(10), TEN(10), NINE(9), EIGHT(8),
+		SEVEN(7), SIX(6), FIVE(5), FOUR(4), THREE(3);
+
+        private final int value;
+
+        Rank(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
 	}
 	
 	public enum Suit
 	{
-		 HEARTS, DIAMONDS, CLUBS, SPADES
+	    HEARTS(4), DIAMONDS(3), CLUBS(2), SPADES(1);
+        private final int value;
+
+        Suit(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
+
 	}
 	
 	private final Rank	rank;
@@ -19,8 +39,9 @@ public class Card
 	    this.rank = rank;
 	    this.suit = suit;
 	}
-	
+
 	public Rank rank()          { return rank; }
 	public Suit suit()          { return suit; }
 	public String toString()    { return rank + " of " + suit; }
+    public int cardValue()      { return rank.getValue() * suit.getValue(); }
 }
