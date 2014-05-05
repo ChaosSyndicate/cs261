b0cdab7aa8573e021df73d51203eba816f665cab
Kevin Bui <bui.kevin@live.com>
2014-05-04 18:47:05 -0700

    Created the play function

--- asgmt02/src/Dealer.java
+++ asgmt02/src/Dealer.java
@@ -24,7 +24,32 @@ public class Dealer
         }
 
         Hand playerHand[] = new Hand[playersNum];
+    public void play(int NumGames, int playersNum, int cardNum, int itemsPerLine, boolean shuffle) {
+        for (int i = 0; i < NumGames; i ++) {
+            if (shuffle) {
+                cardDeck.shuffle();
+            }
 
+            Hand playerHand[] = new Hand[playersNum];
+            for (int j = 0; j < playersNum; j++) {
+                playerHand[j] = new Hand();
+            }
+
+            //Deal Cards for each player
+            for (int j = 0; j < cardNum; j++) {
+                for (int k = 0; k < playersNum; k++) {
+                    playerHand[j].add(dealCard());
+                }
+            }
+
+            for (int j = 0; j < )
+
+
+
+
+        }
+        System.out.println("=== Games Complete ===");
 
     }
+
 }
